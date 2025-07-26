9fdde0b06ce29f5e8080823741116f724aebb339
Blocky30 <161234636+Blocky30@users.noreply.github.com>
2025-07-26 14:13:21 +0200

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -3,3 +3,4 @@ echo -ne "\033]0;IntrudeX.exe\007"
 echo -e "\e[32m"
 python3 main.py
 read -p "Press any key to continue..."
+# lazy coding of me myself and i please roast me on my bad coding back in i think 2024 it was
